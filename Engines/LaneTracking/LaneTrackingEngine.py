48950b968efca404c07dec0a9fda110ec960a7b6
Julian <mail@julian-r.de>
2018-09-07 14:18:44 +0200

    Added image event test

--- Engines/LaneTracking/LaneTrackingEngine.py
+++ Engines/LaneTracking/LaneTrackingEngine.py
@@ -1,4 +1,13 @@
+import cozmo
+import PIL
+
 class LaneTrackingEngine:
 
     def __init__(self):
-        print("Lane Tracking Engine!")
+        print("Processing still image")
+
+
+    def ProcessStillImage(self, stillImage):
+       image = cozmo.world.CameraImage(stillImage)
+       rawImage = image.raw_image
+       print(rawImage)
--- core.py
+++ core.py
@@ -1,10 +1,18 @@
 import cozmo
 from cozmo.util import degrees, distance_mm, speed_mmps
-
-def DriveInSquare(robot: cozmo.robot.Robot):
-   while True:
-       robot.drive_straight(distance_mm(200), speed_mmps(150), False, False, 0).wait_for_completed()
-       robot.turn_in_place(cozmo.util.degrees(92), False, 0, angle_tolerance=degrees(0)).wait_for_completed()
+from Engines.LaneTracking import LaneTrackingEngine
 
 
-cozmo.run_program(DriveInSquare)
+def cozmo_program(robot: cozmo.robot.Robot):
+    # Process still image
+    engine = LaneTrackingEngine.LaneTrackingEngine
+
+    # Grab still image from cozmo
+    robot.add_event_handler(cozmo.world.EvtNewCameraImage, engine.ProcessStillImage())
+
+    robot.camera.image_stream_enabled = True
+
+    while True:
+        pass
+
+cozmo.run_program(cozmo_program)
