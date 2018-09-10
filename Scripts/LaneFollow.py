e805f25257455a40679d3598622f82f72de59b9f
Dominik Heinz <dominik.c.heinz@stud.h-da.de>
2018-09-10 16:48:29 +0200

    Added script folder + added hotkey lib pyhk

---
+++ Scripts/LaneFollow.py
@@ -0,0 +1,29 @@
+import cozmo
+import time
+from Engines.DriveController import DriveController
+from Engines.LaneTracking import LaneTrackingEngine
+
+
+def run(robot: cozmo.robot.Robot):
+    # Setup Lanetracking Engine
+    drive_controller = DriveController.DriveController(robot)
+    engine = LaneTrackingEngine.LaneTrackingEngine(robot, drive_controller)
+
+    # Setup event handlers
+    robot.enable_stop_on_cliff(False)
+    robot.camera.image_stream_enabled = True
+    robot.camera.color_image_enabled = False
+    robot.set_head_light(False)
+    robot.set_head_angle(cozmo.robot.MIN_HEAD_ANGLE + cozmo.util.degrees(4), in_parallel=True)
+    robot.set_lift_height(1.0, in_parallel=True)
+
+    time.sleep(1)
+
+    robot.add_event_handler(cozmo.world.EvtNewCameraImage, engine.process_still_image)
+
+    drive_controller.go()
+
+    print("Battery Voltage:", robot.battery_voltage)
+
+    while True:
+        pass
