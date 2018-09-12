5f6499f1eec8e1d15d7edffd8e9b9f818d70f492
Fabian Faltin <fabian.faltin@stud.h-da.de>
2018-09-12 11:18:07 +0200

    Settings aktualiesiert

--- Settings/CozmoSettings.py
+++ Settings/CozmoSettings.py
@@ -1,2 +1,11 @@
 class Settings:
-    cozmo_dive_speed = 40
+    image_binarization_threshold = 30  # Maximum grey value to be mapped to 0, all above will be mapped to 1
+
+    cozmo_enable_drive = True
+    cozmo_drive_speed = 70
+    cozmo_turn_speed_slow_wheel = 0  # Speed of the slower wheel while turning
+    cozmo_turn_speed_fast_wheel = cozmo_drive_speed * 0.7  # Speed of the faster wheel wile turning
+
+    cozmo_show_cam_live_feed = True
+    cozmo_framerate_limit = 40
+    cozmo_preview_resolution = (1080, 720)
