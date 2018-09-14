5d7d79d918a3b599725eb08759f5f73b9da04804
Dominik Heinz <dominik.c.heinz@stud.h-da.de>
2018-09-14 14:31:24 +0200

    Added detailed pattern to PixelRow class

--- Engines/LaneTracking/PixelRow.py
+++ Engines/LaneTracking/PixelRow.py
@@ -1,12 +1,12 @@
 import numpy
-from Settings.CozmoSettings import Settings
 from Engines.LaneTracking.ImagePreprocessor import ImagePreprocessor
 
-
 class PixelRow:
-    pattern = None
+
+    pattern = numpy.array(0)  # e.g. [1, 0, 1]
+    detailed_pattern = numpy.array(0)  # e.g. [[26, 1],[107, 0],[187, 1]]
 
     def __init__(self, raw_row):
         rle_data = ImagePreprocessor.run_length_encoding(raw_row)
-        rle_data = ImagePreprocessor.cleanup_row_noise(rle_data)
-
+        self.detailed_pattern = ImagePreprocessor.cleanup_row_noise(rle_data)
+        self.pattern = self.detailed_pattern[:, 1]
