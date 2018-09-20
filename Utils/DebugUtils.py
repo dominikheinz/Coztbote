307fe8d6d0aefd6ca58e99720ba9909c3762e90d
Julian <mail@julian-r.de>
2018-09-20 09:18:46 +0200

    Added average and max to timing utils

--- Utils/DebugUtils.py
+++ Utils/DebugUtils.py
@@ -4,6 +4,9 @@ import numpy
 
 class DebugUtils:
 
+    # Value format: [[elapsed time sum, number of times called], [max elapsed time]]
+    timed_functions = {}
+
     @staticmethod
     def start_timer():
         """
@@ -33,7 +36,21 @@ class DebugUtils:
             :param function_name: The timed function name which will be included in the console print
             """
             elapsed_time = time.time() - self.start_time
-            print('{:s} function took {:.3f} ms'.format(function_name, elapsed_time * 1000.0))
+            if function_name in DebugUtils.timed_functions:
+                # Update values for average calculation
+                DebugUtils.timed_functions[function_name][0][0] += elapsed_time * 1000.0
+                DebugUtils.timed_functions[function_name][0][1] += 1
+                if elapsed_time > DebugUtils.timed_functions[function_name][1]:
+                    # Save max value
+                    DebugUtils.timed_functions[function_name][1] = elapsed_time * 1000.0
+
+                avg_value = DebugUtils.timed_functions[function_name][0][0] / DebugUtils.timed_functions[function_name][0][1]
+                max_value = DebugUtils.timed_functions[function_name][1]
+                print('{:s} function took {:.3f} ms (Avg: {:.3f}, Max: {:.3f})'
+                      .format(function_name, elapsed_time * 1000.0, avg_value, max_value))
+            else:
+                DebugUtils.timed_functions[function_name] = [[elapsed_time * 1000.0, 1], elapsed_time * 1000.0]
+                print('{:s} function took {:.3f} ms'.format(function_name, elapsed_time * 1000.0))
 
     @staticmethod
     def unique_count(array):
